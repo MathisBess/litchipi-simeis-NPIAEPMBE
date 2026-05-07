a1c198f3377ac8010b728eca52a8a38632b1b22f
MathisBess <bessonmathis7@gmail.com>
2026-05-07 11:16:38 +0200

    Fix: SDK error

--- example/python/sdk.py
+++ example/python/sdk.py
@@ -1,1 +1,7 @@
-../../sdk/python.py
+import sys
+import os
+
+SDK_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'sdk'))
+sys.path.append(SDK_PATH)
+
+from python import SimeisSDK
