c6c08f05376a915346f9449ec3c6633bbd8c21c1
MathisBess <bessonmathis7@gmail.com>
2026-05-07 11:27:41 +0200

    Feature: Reformat sdk

--- example/python/sdk.py
+++ example/python/sdk.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-SDK_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'sdk'))
+SDK_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "sdk"))
 sys.path.append(SDK_PATH)
 
 from python import SimeisSDK
